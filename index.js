4ca3440a224180494093c2874bd2eed25b64c92e
Carol Alonso <c.alonsolp@gmail.com>
2020-07-17 19:52:31 +0200

    task: forms

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ axios.interceptors.request.use((c) => {
 axios.interceptors.response.use((c) => {
 	setTimeout(() => setSpinner(false), 500);
 	return c.data;
+}, (e) => {
+	if (e.response.status >= 400) {
+		showErrorToast(e);
+	}
+	setSpinner(false);
+	return e;
 })
 
 function setSpinner(show) {
@@ -38,25 +44,50 @@ function createElement(tag, other) {
 }
 
 function showErrorToast(e) {
-	const toast = createElement('div', 'alert alert-danger', undefined, undefined, undefined, JSON.stringify(e));
-	toast.setAttribute('role', 'alert');
+	const toast = createElement('div', { class: 'alert alert-danger', text: e, role: 'alert' });
 	const wrapper = document.getElementById('toast');
 	wrapper.innerHTML = '';
 	wrapper.appendChild(toast);
 }
 
+function getInputValue(id) {
+	return document.getElementById(id).value;
+}
+
+const ids = ['name', 'avatar', 'age', 'grade', 'species'];
+
+function getStudentFormValue() {
+	const student = {};
+	ids.forEach(id => student[id] = getInputValue(id));
+	if (!student.avatar) {
+		student.avatar = "file:///Users/cx02088/Downloads/avataaars.png";
+	}
+	return student;
+}
+
+function createStudent() {
+	clearContainer();
+	container.appendChild(renderForm());
+}
+
+function createInputGroup(type, value, name) {
+	const label = createElement('label', { for: name, text: name[0].toUpperCase() + name.slice(1) });
+	const input = createElement('input', { type: type, value: value, name: name, class: 'form-control', id: name });
+	const group = createElement('div', { children: [label, input], class: 'form-group' });
+	return group;
+}
+
 function renderForm(s) {
-	const nameLabel = createElement('label', { for: 'name', class: 'form-control' });
-	const name = createElement('input', { type: 'text', value: s.name, name: 'name', class: 'form-control' });
-	const nameGroup = createElement('div', { children: [nameLabel, name], class: 'form-group' });
-	const form = createElement('form', { children: [nameGroup], class: 'form-control' });
+	const title = createElement('h1', { text: `${s ? 'Edit' : 'Create'} ${s ? s.name : 'student'}`, class: 'mt-10' });
+	const inputs = ids.map(id => createInputGroup(['age', 'grade'].includes(id) ? 'number' : 'text', s ? s[id] : '', id));
+	const button = createElement('button', { text: s ? 'Edit' : 'Create', onclick: (event) => s ? doEditStudent(event, s) : doCreateStudent(event), class: 'btn btn-primary' });
+	const form = createElement('form', { children: [title, ...inputs, button] });
 	return form;
 }
 
 function editStudent(s) {
-	console.log(s);
 	clearContainer();
-	/*container.appendChild(renderForm(s));*/
+	container.appendChild(renderForm(s));
 }
 
 function renderStudent(student) {
@@ -65,9 +96,7 @@ function renderStudent(student) {
 	const btn = createElement('button', { class: 'btn btn-primary', onclick: () => editStudent(student), text: 'Edit me' });
 	const body = createElement('div', { class: 'card-body', children: [h5, p, btn] })
 	const img = createElement('img', { src: student.avatar, class: 'card-img-top' });
-	console.log(img);
-	console.log(body);
-	const element = createElement('div', { class: 'card', style: "width: 18rem;", children: [img, body] });
+	const element = createElement('div', { class: 'card', style: "width: 15rem;", children: [img, body] });
 	return element;
 }
 
